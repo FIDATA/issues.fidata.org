5195c4a591d670fe163c8f9467fe2e1e8db6d164
Basil Peace <grv87@yandex.ru>
2019-12-24 15:38:17 +0300

    feat: update mantisbt

--- download.cmd
+++ download.cmd
@@ -3,5 +3,5 @@ REM SPDX-License-Identifier: Apache-2.0
 mkdir build
 cd build
 rm *.zip
-curl --user %ARTIFACTORY_USERNAME%:%ARTIFACTORY_PASSWORD% --output issues.zip https://fidata.jfrog.io/fidata/composer-local/fidata/mantisbt-0.1.4.zip
+curl --user %ARTIFACTORY_USERNAME%:%ARTIFACTORY_PASSWORD% --output issues.zip https://fidata.jfrog.io/fidata/composer-local/fidata/mantisbt-0.2.0.zip
 cd ..
